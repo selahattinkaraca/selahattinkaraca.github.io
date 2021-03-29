73d7192602af967837c7476581f47d30f3d772d6
selahattin-krc <selahattin.karaca@marun.edu.tr>
2021-03-29 20:13:59 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,4 +1,11 @@
 alert("Merhaba dunya");
+function merhaba() {
+    alert("Merhaba");
+}
+
+function renkdegistir() {
+    document.getElementById("anadiv").style.color = 'blue';
+}
 function oyunsayfasını_Ac(){
   window.location.href = "https://selahattinkaraca.github.io/oyun";
 }
